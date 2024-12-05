88333e0175d86423d4e248c712ef33a97140f7cd
SivakumarReddy143 <mshivakumarreddy78@gmail.com>
2024-12-05 07:46:38 +0530

    created s3 bucket

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -3,10 +3,13 @@ import sys
 from datetime import datetime
 from networksecurity.constant import training_pipeline
 class TrainingPipelineConfig:
-    def __init__(self):
+    def __init__(self,timestamp=datetime.now()):
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_NAME
         self.artifact_dir = os.path.join(self.artifact_name,datetime.now().strftime('%m_%d_%Y_%H_%M_%S'))
+        self.model_dir = os.path.join("final_model")
+        self.timestamp: str = timestamp
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
